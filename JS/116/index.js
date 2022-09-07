bd569a8539d68fd94906e68c29299b2b109c0754
Pavel <designdpcompany@gmail.com>
2022-09-07 16:57:17 +0300

    89-95 done

---
+++ JS/116/index.js
@@ -0,0 +1,10 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  const p = s[0];
+  return s.sort();
+
+  // Only change code above this line
+}
+console.log(editInPlace());
+console.log(s)
